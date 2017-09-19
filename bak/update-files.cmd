4326033b2f86091561e26137cc92e61f11e73762
Fish Li <liqifeng0503@163.com>
2017-09-19 19:56:52 +0800

    更新配置文件

--- bak/update-files.cmd
+++
@@ -1,5 +0,0 @@
-copy ..\src\SpecChecker.ConsoleApp\bin\task-*.xml . /y
-copy ..\src\SpecChecker.ConsoleApp\bin\*.config . /y
-copy ..\src\SpecChecker.ConsoleApp\bin\*.cmd . /y
-copy ..\src\SpecChecker.ConsoleApp\bin\*.txt . /y
-pause
