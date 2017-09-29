206f99105d6d69b5d2904246310d63f3eea8eb8d
Fish Li <liqifeng0503@163.com>
2017-09-29 14:37:02 +0800

    优化代码扫描的排除标记

--- src/SpecChecker.CoreLibrary/Properties/AssemblyVersion.cs
+++ src/SpecChecker.CoreLibrary/Properties/AssemblyVersion.cs
@@ -15,4 +15,4 @@ using System.Runtime.InteropServices;
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.17.927.1015")]
+[assembly: AssemblyFileVersion("1.17.929.1133")]
