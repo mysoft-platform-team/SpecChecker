00fc5341849ac1c0a88e5991313993c0466b1cdc
Fish Li <liqifeng0503@163.com>
2018-01-03 16:42:23 +0800

    调用ReSharper dotCover时增加重试机制

--- src/SpecChecker.ScanLibrary/UnitTest/UnitTestWorker.cs
+++ src/SpecChecker.ScanLibrary/UnitTest/UnitTestWorker.cs
@@ -82,30 +82,53 @@ namespace SpecChecker.ScanLibrary.UnitTest
             // 单元测试结果文件，包含通过率数据
             // 为了防止项目同名，单元测试生成的结果文件和项目文件保存在一起，而不是直接保存到临时目录
             string resultFile = Path.Combine(Path.GetDirectoryName(_projectFile), @"TestResult.xml");
-            if( File.Exists(resultFile) )
-                File.Delete(resultFile);
-
             string coverFile = Path.Combine(Path.GetDirectoryName(_projectFile), @"CoverResult.dcvr");
-            if( File.Exists(coverFile) )
-                File.Delete(coverFile);
+
+            int retryTime = 0;
+
+            while( retryTime <= 3 ) { 
+
+                if( File.Exists(resultFile) )
+                    File.Delete(resultFile);
+
+                if( File.Exists(coverFile) )
+                    File.Delete(coverFile);
 
 
-            CoverageParams cp = new CoverageParams();
-            cp.TargetExecutable = _nunitPath;
-            cp.TargetArguments = string.Format("\"{0}\" --result \"{1}\"  --inprocess", _projectFile, resultFile);
-            cp.TargetWorkingDir = Path.Combine(Path.GetDirectoryName(_projectFile), "bin");
-            cp.Output = coverFile;
+                CoverageParams cp = new CoverageParams();
+                cp.TargetExecutable = _nunitPath;
+                cp.TargetArguments = string.Format("\"{0}\" --result \"{1}\"  --inprocess", _projectFile, resultFile);
+                cp.TargetWorkingDir = Path.Combine(Path.GetDirectoryName(_projectFile), "bin");
+                cp.Output = coverFile;
 
-            string argFileName = string.Format("UnitTest-cover-args-{0}.xml",
-                                    Path.GetFileNameWithoutExtension(_projectFile).Replace(".", "_"));
-            string args1File = _context.WriteTempFile(cp, argFileName);
+                string argFileName = string.Format("UnitTest-cover-args-{0}.xml",
+                                        Path.GetFileNameWithoutExtension(_projectFile).Replace(".", "_"));
+                string args1File = _context.WriteTempFile(cp, argFileName);
 
-            // 调用 ReSharper dotCover ，执行单元测试，并生成代码覆盖率快照文件
-            ExecuteResult result = CommandLineHelper.Execute(_dotCoverPath, string.Format("cover \"{0}\"", args1File));
-            ConsoleWrite(result.ToString());
+                // 调用 ReSharper dotCover ，执行单元测试，并生成代码覆盖率快照文件
+                ExecuteResult result = CommandLineHelper.Execute(_dotCoverPath, string.Format("cover \"{0}\"", args1File));
+                ConsoleWrite(result.ToString());
 
-            result.FillBaseInfo();      // 设置日志的时间，当前机器名
-            _context.WriteTempFile(result, "UnitTest-RunCover-Console-" + Path.GetFileNameWithoutExtension(_projectFile) + ".txt");
+                result.FillBaseInfo();      // 设置日志的时间，当前机器名
+                _context.WriteTempFile(result, "UnitTest-RunCover-Console-" + Path.GetFileNameWithoutExtension(_projectFile) + ".txt");
+
+
+                // ReSharper dotCover 时常会出现BUG，现象为不能生成代码覆盖率快照文件，例如下面的执行消息：
+
+                //Results (nunit3) saved as D:\TFS\10_5_10_96\Prod\ERP-V1.0\40_成本系统\01_主干-开发分支\88 公共类库\Mysoft.Cbxt.Common.UnitTest\TestResult.xml
+                //[JetBrains dotCover] Coverage session finished [2018/1/3 6:06:05]
+                //[JetBrains dotCover] Coverage session finished but no snapshots were created.
+
+                // 目前的解决方法是，运行 dotCover 之后再检查是否已生成快照文件，如果文件没有生成就重新执行
+                // 重试的最大次数为 3
+
+                if( File.Exists(coverFile) )
+                    break;
+                else {
+                    System.Threading.Thread.Sleep(3000);
+                    retryTime++;
+                }
+            }
 
 
 
